af7f2d0a9bce89933c56bf4bd5a1ded4a542f42d
student <student@g2.iste444.com>
2022-04-21 20:30:07 -0400

    Add book functionality

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Table, Button, Form} from 'react-bootstrap';
 import '../styles/Bootstrap.scss';
 import '../styles/Theme.css';
@@ -13,6 +13,14 @@ export default function Dashboard() {
     const [books, setBooks] = useState([]);
     const [bookCount, setBookCount] = useState(0);
     const token = localStorage.getItem("token");
+    const idRef = useRef([]);
+    const titleRef = useRef([]);
+    const descriptionRef = useRef([]);
+    const isbnRef = useRef([]);
+    const pagesRef = useRef([]);
+    const yearRef = useRef([]);
+    const publisherRef = useRef([]);
+    const checkedOutByRef = useRef([]);
 
     // Run on document load
     useEffect(() => {
@@ -45,11 +53,39 @@ export default function Dashboard() {
     }
 
     async function addBook() {
+        console.log(titleRef.current[0].value)
         try {
-            const response = await axios.post("http://localhost:8080/book", {
-                token: token
+            await axios.post("http://localhost:8080/book", {
+                token: token,
+                title: titleRef.current[0].value,
+                description: descriptionRef.current[0].value,
+                isbn: isbnRef.current[0].value,
+                pages: pagesRef.current[0].value,
+                year: yearRef.current[0].value,
+                publisher: publisherRef.current[0].value,
+                checkedoutby: checkedOutByRef.current[0].value
             })
-            setBookCount(response.data.length);
+            window.location.reload();
+        } catch (error) {
+            console.log(error)
+            setMessage("Could not get the list of books! Please Refresh!")
+            setShow(true);
+        }
+    }
+
+    async function editBook(id) {
+        try {
+            await axios.put("http://localhost:8080/book", {
+                token: token,
+                title: titleRef.current[id].value,
+                description: descriptionRef.current[id].value,
+                isbn: isbnRef.current[id].value,
+                pages: pagesRef.current[id].value,
+                year: yearRef.current[id].value,
+                publisher: publisherRef.current[id].value,
+                checkedoutby: checkedOutByRef.current[id].value
+            })
+            window.location.reload();
         } catch (error) {
             console.log(error)
             setMessage("Could not get the list of books! Please Refresh!")
@@ -82,31 +118,31 @@ export default function Dashboard() {
                     <tbody>
                         <tr>
                             <td>
-                                <Form.Control readOnly id="0-title" value={bookCount + 1} />
+                                <Form.Control readOnly id="0-title" ref={el => {idRef.current[0] = el}} value={bookCount + 1} />
                             </td>
                             <td>
-                                <Form.Control id="0-title" defaultValue='' />
+                                <Form.Control id="0-title" ref={el => {titleRef.current[0] = el}} defaultValue='' />
                             </td>
                             <td>
-                                <Form.Control id="0-description" defaultValue='' />
+                                <Form.Control id="0-description" ref={el => {descriptionRef.current[0] = el}} defaultValue='' />
                             </td>
                             <td>
-                                <Form.Control id="0-isbn" defaultValue='' />
+                                <Form.Control id="0-isbn" ref={el => {isbnRef.current[0] = el}} defaultValue='' />
                             </td>
                             <td>
-                                <Form.Control id="0-pages" defaultValue='' />
+                                <Form.Control id="0-pages" ref={el => {pagesRef.current[0] = el}} defaultValue='' />
                             </td>
                             <td>
-                                <Form.Control id="0-year" defaultValue='' />
+                                <Form.Control id="0-year" ref={el => {yearRef.current[0] = el}} defaultValue='' />
                             </td>
                             <td>
-                                <Form.Control id="0-publisher" defaultValue='' />
+                                <Form.Control id="0-publisher" ref={el => {publisherRef.current[0] = el}} defaultValue='' />
                             </td>
                             <td>
-                                <Form.Control id="0-checkedoutby" defaultValue='' />
+                                <Form.Control id="0-checkedoutby" ref={el => {checkedOutByRef.current[0] = el}} defaultValue='' />
                             </td>
                             <td>
-                                <Button><AiOutlinePlus /></Button>
+                                <Button onClick={addBook}><AiOutlinePlus /></Button>
                             </td>
                         </tr>
                         {
@@ -114,31 +150,31 @@ export default function Dashboard() {
                                 return (
                                     <tr key={book.bookid}>
                                         <td>
-                                            <Form.Control readOnly id={book.bookid + "-bookid"} value={book.bookid} />
+                                            <Form.Control readOnly ref={el => {idRef.current[book.bookid] = el}} value={book.bookid} />
                                         </td>
                                         <td>
-                                            <Form.Control id={book.title + "-title"} defaultValue={book.title} />
+                                            <Form.Control ref={el => {titleRef.current[book.bookid] = el}} defaultValue={book.title} />
                                         </td>
                                         <td>
-                                            <Form.Control id={book.description + "-description"} defaultValue={book.description} />
+                                            <Form.Control ref={el => {descriptionRef.current[book.bookid] = el}} defaultValue={book.description} />
                                         </td>
                                         <td>
-                                            <Form.Control id={book.isbn + "-isbn"} defaultValue={book.isbn} />
+                                            <Form.Control ref={el => {isbnRef.current[book.bookid] = el}} defaultValue={book.isbn} />
                                         </td>
                                         <td>
-                                            <Form.Control id={book.pages + "-pages"} defaultValue={book.pages} />
+                                            <Form.Control ref={el => {pagesRef.current[book.bookid] = el}} defaultValue={book.pages} />
                                         </td>
                                         <td>
-                                            <Form.Control id={book.year + "-year"} defaultValue={book.year} />
+                                            <Form.Control ref={el => {yearRef.current[book.bookid] = el}} defaultValue={book.year} />
                                         </td>
                                         <td>
-                                            <Form.Control id={book.publisher + "-publisher"} defaultValue={book.publisher} />
+                                            <Form.Control ref={el => {publisherRef.current[book.bookid] = el}} defaultValue={book.publisher} />
                                         </td>
                                         <td>
-                                            <Form.Control id={book.checkedoutby + "-checkedoutby"} defaultValue={book.checkedoutby} />
+                                            <Form.Control ref={el => {checkedOutByRef.current[book.bookid] = el}} defaultValue={book.checkedoutby} />
                                         </td>
                                         <td>
-                                            <Button><AiTwotoneEdit /></Button>
+                                            <Button onClick={() => editBook(book.bookid)}><AiTwotoneEdit /></Button>
                                         </td>
                                         <td>
                                             <Button><AiFillDelete /></Button>
